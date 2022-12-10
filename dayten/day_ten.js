26d73956304f90533f7190661021f5abc6a071eb
Tyler-Gates <65058265+Tyler-Gates@users.noreply.github.com>
2022-12-10 00:26:25 -0600

    Day ten complete!

---
+++ dayten/day_ten.js
@@ -0,0 +1,91 @@
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+function ingestFile() {
+    var total = 0;
+    var input = fs.readFileSync(".\\input.txt", 'utf-8').split(/\r\n/).filter(function (c) { return c; });
+    return input;
+}
+function getSignalSum(input) {
+    var total = 0;
+    var x = 1;
+    var ticks = 0;
+    for (var i = 0; i < input.length; i++) {
+        var temp = input[i].split(' ').filter(function (c) { return c; });
+        if (temp[0] == "noop") {
+            ticks++;
+            if ((ticks - 20) % 40 == 0 && ticks <= 220) {
+                total += x * ticks;
+            }
+        }
+        else {
+            ticks++;
+            if ((ticks - 20) % 40 == 0 && ticks <= 220) {
+                total += x * ticks;
+            }
+            ticks++;
+            if ((ticks - 20) % 40 == 0 && ticks <= 220) {
+                total += x * ticks;
+            }
+            x += +temp[1];
+        }
+    }
+    console.log("The total is: " + total);
+}
+function createImage(input) {
+    var mystery = [];
+    var currentLine = "";
+    var ticks = 0;
+    var sprite = 2;
+    for (var i = 0; i < input.length; i++) {
+        var temp = input[i].split(' ').filter(function (c) { return c; });
+        if (temp[0] == "noop") {
+            ticks++;
+            //check
+            if (ticks >= sprite - 1 && ticks <= sprite + 1) {
+                currentLine = currentLine.concat("#");
+            }
+            else {
+                currentLine = currentLine.concat('.');
+            }
+            if (ticks % 40 == 0) {
+                mystery.push(currentLine);
+                currentLine = "";
+                ticks = 0;
+            }
+        }
+        else {
+            ticks++;
+            //check
+            if (ticks >= sprite - 1 && ticks <= sprite + 1) {
+                currentLine = currentLine.concat("#");
+            }
+            else {
+                currentLine = currentLine.concat('.');
+            }
+            if (ticks % 40 == 0) {
+                mystery.push(currentLine);
+                currentLine = "";
+                ticks = 0;
+            }
+            ticks++;
+            //check
+            if (ticks >= sprite - 1 && ticks <= sprite + 1) {
+                currentLine = currentLine.concat("#");
+            }
+            else {
+                currentLine = currentLine.concat('.');
+            }
+            if (ticks % 40 == 0) {
+                mystery.push(currentLine);
+                currentLine = "";
+                ticks = 0;
+            }
+            sprite += +temp[1];
+        }
+    }
+    console.log(mystery);
+}
+var input = ingestFile();
+getSignalSum(input);
+createImage(input);
---
+++ dayten/day_ten.ts
@@ -0,0 +1,94 @@
+import * as fs from 'fs';
+
+
+function ingestFile(): string[] {
+    var total: number = 0;
+    var input = fs.readFileSync(".\\input.txt", 'utf-8').split(/\r\n/).filter(c => c);
+    return input;
+}
+
+function getSignalSum(input: string[]) {
+    var total = 0;
+    var x = 1;
+    var ticks = 0;
+    for (var i = 0; i < input.length; i++){
+        var temp = input[i].split(' ').filter(c => c);
+        if(temp[0] == "noop") {
+            ticks++;
+            if ( (ticks - 20) % 40 == 0 && ticks  <= 220) {
+                total += x * ticks;
+            }
+        }
+        else {
+            ticks++;
+            if ( (ticks - 20) % 40 == 0 && ticks  <= 220)  {
+                total += x * ticks;
+            }
+            ticks++;
+            if ( (ticks - 20) % 40 == 0 && ticks  <= 220)  {
+                total += x * ticks;
+            }
+            x += +temp[1];
+        }
+    }
+    console.log("The total is: " + total);
+}
+
+function createImage(input: string[]) {
+    var mystery: Array<string> = [];
+    var currentLine = "";
+    var ticks = 0;
+    var sprite = 2;
+    for (var i = 0; i < input.length; i++){
+        var temp = input[i].split(' ').filter(c => c);
+        if(temp[0] == "noop") {
+            ticks++
+            //check
+            if (ticks >= sprite - 1 && ticks <= sprite + 1) {
+                currentLine = currentLine.concat("#");
+            }
+            else {
+                currentLine = currentLine.concat('.');
+            }
+            if (ticks % 40 == 0) {
+                mystery.push(currentLine)
+                currentLine = "";
+                ticks = 0;
+            }
+        }
+        else {
+            ticks++
+            //check
+            if (ticks >= sprite - 1 && ticks <= sprite + 1) {
+                currentLine = currentLine.concat("#");
+            }
+            else {
+                currentLine = currentLine.concat('.');
+            }
+            if (ticks % 40 == 0) {
+                mystery.push(currentLine)
+                currentLine = "";
+                ticks = 0;
+            }
+            ticks++
+            //check
+            if (ticks >= sprite - 1 && ticks <= sprite + 1) {
+                currentLine = currentLine.concat("#");
+            }
+            else {
+                currentLine = currentLine.concat('.');
+            }
+            if (ticks % 40 == 0) {
+                mystery.push(currentLine)
+                currentLine = "";
+                ticks = 0;
+            }
+            sprite += +temp[1];
+        }
+    }
+    console.log(mystery);
+}
+
+var input = ingestFile();
+getSignalSum(input);
+createImage(input);
